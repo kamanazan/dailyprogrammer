3de9ed6cf3195cb6ca06e6ec3c4dd59f3072798b
kamanazan <fauzan.rosyadi@gmail.com>
2014-08-20 08:10:11 +0700

    update setelah sekian lama

--- ch_8_i.py
+++ ch_8_i.py
@@ -5,6 +5,26 @@ angka = {"1":"satu","2":"dua","3":"tiga","4":"empat","5":"lima","6":"enam","7":"
 		"8":"delapan","9":"sembilan"}
 
 digit = {2:"puluh",3:"ratus",4:"ribu",5:"puluh",7:"juta"}
+'''
+for x in [9,6,3,2,1]:
+	if num < 10 ** x:
+		if x == 1 and num < 20:
+			kpt = "belas"
+			break
+		else:
+			kpt = digit[x]
+			break
+return kpt,x
+
+def to_word(num):
+	if num < 10:
+		return angka[num]
+	else:
+		kpt,x = getKelipatan(num)
+		process_num = num / 10 ** x
+		sisa = num - (process_num * 10 ** x)
+		return 	to_word(process_num) + kpt + to_word(sisa)
+'''
 print "digit:",digit[len(num)]
 count = len(num)
 if len(num) == 2 and num[0] == "1":
